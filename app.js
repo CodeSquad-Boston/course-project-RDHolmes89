705149460c4c508463f3b841a4199722a3415b60
rdholmes89 <rochelleholmes89@gmail.com>
2022-04-12 09:11:07 -0400

    Finished hw 10 server up and running

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-// const { response } = require('express');
-// const express = require('express');
-// const app = express();
-// const PORT = 3000;
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const PORT = 3000;
+
+app.use(morgan('combined'));
+
+//ROUTES
+app.get('/', (req, res) => {
+    res.send('This is the homepage.')
+});
+
+app.get('/about', (req, res) => {
+    res.send('This route points to the About page.');
+});
+
+app.get('/login', (req, res) => {
+    res.send('This route points to the Login page.');
+});
+
+app.get('/admin-console', (req, res) => {
+    res.send('This route points to the Admin Console page.');
+});
+
+app.get('/admin-console/create-book', (req, res) => {
+    res.send('This route points to the Create page.');
+});
 
 
-
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on ${PORT}`);
-// })
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+})
