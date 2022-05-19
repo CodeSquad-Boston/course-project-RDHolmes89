f234376d63b19cb2bc3f593bfb72c451fdadf19d
rdholmes89 <rochelleholmes89@gmail.com>
2022-05-19 15:42:34 -0400

    book controller complete

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -8,23 +8,24 @@ const Comic = require('../models/comic-model');
 //     books: app.get('/books/:id', (req, res) => {
 //         res.render('pages/books');
 //     })
-   
+
 // }
 module.exports = {
     books: (req, res) => {
         const { _id } = req.params;
         Comic.findOne({_id: _id}, (error, foundComic) => {
-            if(error) {     
+            if (error) {
+                return error;
             } else {
                 res.render('pages/books', {
                     foundBook: foundComic
+                });
+            }
         });
-    }
-        }); 
-},
+    },
     book_create: (req, res) => {
-        const {title, author, publisher, genre, pages, rating, synopsis, image} = req.body;
-        const newComic = new Comic ({
+        const { title, author, publisher, genre, pages, rating, synopsis, image } = req.body;
+        const newComic = new Comic({
             title: title,
             author: author,
             publisher: publisher,
@@ -36,33 +37,40 @@ module.exports = {
         });
         newComic.save();
         res.redirect('/admin-console');
-    
-},
-book_update: (request, response) => {
+    },
+    book_update: (req, res) => {
 
-    const {_id} = request.params;
+        const { _id } = req.params;
 
-    const {title, author, publisher, genre, pages, rating, synopsis, image} = request.body;
-
-    const foundBook = siteData.find(book => book._id === String(_id));
-
-    foundBook.title = title;
-    foundBook.author = author;
-    foundBook.publisher = publisher;
-    foundBook.genre = genre;
-    foundBook.pages = pages;
-    foundBook.rating = rating;
-    foundBook.synopsis = synopsis;
-    foundBook.image = image;
-
-    response.redirect("/admin-console");
-  },
+        const { title, author, publisher, genre, pages, rating, synopsis, image } = req.body;
+        Comic.findByIdAndUpdate(_id, {$set: {
+                title: title,
+                author: author,
+                publisher: publisher,
+                genre: genre,
+                pages: pages,
+                rating: rating,
+                synopsis: synopsis,
+                image: image
+            }
+        }, { new: true }, error => {
+            if (error) {
+                return error;
+            } else {
+                res.redirect("/admin-console");
+            }
+        });
+    },
     book_delete: (req, res) => {
         const { _id } = req.params;
-        const foundBook = siteData.find(book => book._id === _id);
-        const index = siteData.indexOf(foundBook);
-        siteData.splice(index, 1);
-        res.redirect('/admin-console'); 
-        }
-    
+        Comic.deleteOne({_id: _id}, error => {
+            if (error) {
+                return error;
+            } else {
+                res.redirect('/admin-console');
+            }
+        });
+
+    }
+
 }
